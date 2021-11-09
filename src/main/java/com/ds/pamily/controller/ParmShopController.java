32ed9a2d92990fdd56c831696b6064732df1249a
dd2ddoo <86641841+dd2ddoo@users.noreply.github.com>
2021-11-09 17:52:00 +0900

    itemshop

--- src/main/java/com/ds/pamily/controller/ParmShopController.java
+++ src/main/java/com/ds/pamily/controller/ParmShopController.java
@@ -1,22 +1,32 @@
 package com.ds.pamily.controller;
 
+import com.ds.pamily.entity.Item;
+import com.ds.pamily.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/sample/")
 public class ParmShopController {
+    final ItemRepository itemRepository;
 
     @PreAuthorize("permitAll()")
     @GetMapping("/itemshop")
-    public void exItemShop() {
+    public String exItemShop(Model model){
+        List<Item> itemList = itemRepository.findAll();
+        model.addAttribute("itemList", itemList);
+        log.info("item>>>>>>" + itemList.size());
         log.info("exitemshop..........");
+        return "sample/itemshop";
     }
 
     @PreAuthorize("permitAll()")
