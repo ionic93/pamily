77589e71b2c314ce58fe03cf14da06aa7b5285c3
dd2ddoo <86641841+dd2ddoo@users.noreply.github.com>
2021-11-11 17:10:01 +0900

    itemshop

--- src/main/java/com/ds/pamily/controller/ParmController.java
+++ src/main/java/com/ds/pamily/controller/ParmController.java
@@ -1,25 +1,11 @@
 package com.ds.pamily.controller;
 
-import com.ds.pamily.dto.PageRequestDTO;
-import com.ds.pamily.dto.ParmDTO;
-import com.ds.pamily.entity.ItemCate;
-import com.ds.pamily.entity.Member;
-import com.ds.pamily.entity.ParmImage;
-import com.ds.pamily.service.MemberService;
-import com.ds.pamily.service.ParmImageService;
 import com.ds.pamily.service.ParmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Locale;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
 @Controller
