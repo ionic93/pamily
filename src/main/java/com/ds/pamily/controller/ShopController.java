12c056d8c4f5a463511e883409bd2beb620edf8a
ionic93 <86641843+ionic93@users.noreply.github.com>
2021-11-15 17:02:27 +0900

    shop1

--- src/main/java/com/ds/pamily/controller/ShopController.java
+++ src/main/java/com/ds/pamily/controller/ShopController.java
@@ -2,6 +2,8 @@ package com.ds.pamily.controller;
 
 import com.ds.pamily.dto.PageRequestDTO;
 import com.ds.pamily.dto.ShopDTO;
+import com.ds.pamily.entity.ShopCate;
+import com.ds.pamily.service.ShopCateService;
 import com.ds.pamily.service.ShopService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
@@ -14,22 +16,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/shop/")
 public class ShopController {
     private final ShopService shopService;
+    private final ShopCateService shopCateService;
 
     @PreAuthorize("permitAll()")
     @GetMapping("/shop")
-    public String exItemShop(Model model) throws JsonProcessingException {
-        log.info("exitemshop..........");
-        return "shop/shop";
-    }
-
-    @PreAuthorize("permitAll()")
-    @GetMapping("/exshop")
     public void exshop(PageRequestDTO pageRequestDTO, Model model) {
         log.info("exitemshop..........");
         log.info("pageRequestDTO: " + pageRequestDTO);
@@ -37,16 +35,15 @@ public class ShopController {
     }
 
     @PreAuthorize("permitAll()")
-    @GetMapping("/exshopreg")
+    @GetMapping("/shopreg")
     public void exshopReg() {
-        log.info("exShopReg...");
+        log.info("ShopReg...");
     }
 
     @PreAuthorize("permitAll()")
-    @PostMapping("/exshopreg")
+    @PostMapping("/shopreg")
     public String exshopRegPost(ShopDTO shopDTO, RedirectAttributes redirectAttributes) {
         log.info("shopDTO..."+shopDTO);
-
         Long sid = shopService.shopRegister(shopDTO);
         redirectAttributes.addFlashAttribute("msg",sid);
         return "redirect:/exshop";
--- src/main/java/com/ds/pamily/repository/ShopRepository.java
+++ src/main/java/com/ds/pamily/repository/ShopRepository.java
@@ -1,13 +1,15 @@
 package com.ds.pamily.repository;
 
 import com.ds.pamily.entity.Shop;
+import com.ds.pamily.repository.search.SearchShopRepository;
+import com.querydsl.core.BooleanBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ShopRepository extends JpaRepository<Shop,Long>, QuerydslPredicateExecutor<Shop> {
+public interface ShopRepository extends JpaRepository<Shop,Long>, SearchShopRepository {
     @Query("select s, min(si), count(distinct r) from Shop s " +
             " left outer join ShopImage si on si.shop = s " +
             " left outer join ShopReply r on r.shop = s group by s")
