534e9acbbd8f5b5753d85388ecf2423d4bc840fd
YEOMInSik <86641874+YEOMInSik@users.noreply.github.com>
2021-11-19 14:28:18 +0900

    2021/11/19 insik

--- src/main/java/com/ds/pamily/controller/BestController.java
+++ src/main/java/com/ds/pamily/controller/BestController.java
@@ -1,9 +1,14 @@
 package com.ds.pamily.controller;
 
+import com.ds.pamily.dto.PageRequestDTO;
+import com.ds.pamily.dto.PageResultDTO;
+import com.ds.pamily.dto.PostDTO;
+import com.ds.pamily.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,9 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/sample/")
 public class BestController {
+    private final PostService postService;
+
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/best")
-    public void exbest() {
-        log.info("exbest..........");
+    public void exbest(PageRequestDTO pageRequestDTO, Model model) {
+        PageResultDTO<PostDTO, Object[]> result = postService.getList(pageRequestDTO);
+        log.info("result.getDtoList()>>>>>>>>>>>" + result.getDtoList());
+        model.addAttribute("msg" , result.getDtoList());
+        log.info( "model >>>>>>" + model );
+        log.info( "result1>>>>>>" + result );
+        log.info("result2>>>>>>>>" + result.getDtoList());
     }
 }
