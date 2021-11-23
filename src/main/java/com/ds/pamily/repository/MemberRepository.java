38594b6d616522b2241e3d760f07d6004544bae4
jminseok <job1287@naver.com>
2021-11-23 18:13:54 +0900

    JMS 1123_MemberRepository

--- src/main/java/com/ds/pamily/repository/MemberRepository.java
+++ src/main/java/com/ds/pamily/repository/MemberRepository.java
@@ -1,16 +1,18 @@
 package com.ds.pamily.repository;
 
 import com.ds.pamily.entity.Member;
+import com.querydsl.core.BooleanBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
     @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("select m from Member m where m.email = :email and m.fromSocial = :social ")
     Optional<Member> findByEmail(String email, boolean social);
-
-
 }
